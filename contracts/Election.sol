7cb1c3d3ca773b1b5492c58df4163eb04fb1f1c3
ablazleon <ablazleon@gmail.com>
2019-06-04 13:42:31 +0200

    truffle unbox pet-shop

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,29 @@ pragma solidity ^0.5.8;
 
 contract Election {
 
-    // Store candidate
-    // Read candidate
-    string public candidate;
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCOunt;
+    }
+
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
     //Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    /** @dev Adds a candidate.
+      * @param _name Candidate name.
+      */
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
