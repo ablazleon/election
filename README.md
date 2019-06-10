7d95ce200c55e4d27cdbab94014338616dc8e08d
ablazleon <ablazleon@gmail.com>
2019-06-10 20:11:28 +0200

    Vote

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 Example of voting using smart contracts and Ethereum
 
 PS: metamask disable privacy option for displaying the account!!!
+
+For testing it:
+
+Election.deployed().then(function(i) { app = i })
+
+web3.eth.getAccounts()
+
+## 1. How to vote in behalf of a candidate?
+
+// app.vote(1, {from: web3.eth.getCoinbase()})
+
+
+web3.eth.getAccounts().then(function(p){ap = p})
+app.vote(1,{from: ap[0]})
+
+truffle migrate --reset
+
+
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -6,8 +6,11 @@ contract Election {
     struct Candidate {
         uint id;
         string name;
-        uint voteCOunt;
+        uint voteCount;
     }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
 
     // Store Candidates
     // Fetch Candidate
@@ -26,6 +29,21 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidatesCount
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
     }
 }
 
